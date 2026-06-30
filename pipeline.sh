471a671fe5959dcc0cddd38a47c3c6338b90aec6
Datreusss <134525884+Datreuss@users.noreply.github.com>
2026-06-29 23:40:07 -0400

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -7,7 +7,7 @@ echo "🔍 Validando configuración de Docker Compose..."
 docker compose config
 
 if [ $? -ne 0 ]; then
-    echo "❌ Error en la sintaxis de Docker Compose. Abortando."
+    echo " Error en la sintaxis de Docker Compose. Abortando."
     exit 1
 fi
 
@@ -16,12 +16,12 @@ echo "⚙️ Construyendo imágenes y levantando el entorno simulado..."
 docker compose up -d --build
 
 if [ $? -eq 0 ]; then
-    echo "✅ ¡Entorno desplegado con éxito!"
-    echo "📊 Monitoreo listo:"
+    echo "¡Entorno desplegado con éxito!"
+    echo " Monitoreo listo:"
     echo "   - Microservicio Java"
     echo "   - Prometheus: http://localhost:9090"
     echo "   - Grafana Dashboard: http://localhost:3000 (User/Pass: admin/admin)"
 else
-    echo "❌ Hubo un error al levantar los contenedores."
+    echo " Hubo un error al levantar los contenedores."
     exit 1
 fi
